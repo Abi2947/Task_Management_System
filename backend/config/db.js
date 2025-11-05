a3b1860e98d50e2f8a12f804352127baaf6f0a0c
abi2947 <captainphillips70@gmail.com>
2025-11-05 15:36:55 +0545

    Connection with MongoDB Database

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+// Connect to MongoDB (use env for URI to keep it secure)
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("DB Connection Error:", err.message);
+    setTimeout(commectDB, 5000);
+  }
+};
+
+module.exports = connectDB;
